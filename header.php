8b44ca9c6dc6d9922c533c6d27132ae8e28799a7
bestand <kentaro.arai@bestand.jp>
2022-04-02 21:59:02 +0900

    gtag added

--- header.php
+++ header.php
@@ -2,6 +2,9 @@
 <html <?php language_attributes(); ?>>
 
 <head>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WHDSS39');</script>
+	<!-- End Google Tag Manager -->
 	<title>【事務×GYM】 | 事務職を目指す方・事務スキル向上を目指す方のための学習サイト</title>
 	<meta name="description" content="事務職を目指す方・事務のスキルアップを目指す方のための学習サイト"/>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
@@ -20,6 +23,9 @@
 </head>
 
 <body <?php body_class(); ?>>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WHDSS39"	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
 	
 	<header>
 		<div class="site-title">
